833f40a60d30a4398d99ad515824cb7f8c90c3f5
scicluna <88948700+scicluna@users.noreply.github.com>
2023-04-28 10:23:51 -0400

    trying without server rendering

--- server/server.js
+++ server/server.js
@@ -15,11 +15,11 @@ app.use(cors({
   allowedHeaders: ['Content-Type']
 }))
 
-app.use(express.static(path.join(__dirname, '../client/dist')))
+// app.use(express.static(path.join(__dirname, '../client/dist')))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+// });
 
 app.use(routes);
 
