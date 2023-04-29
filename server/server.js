97fc4038c6f7d117172709654b15777966523663
scicluna <88948700+scicluna@users.noreply.github.com>
2023-04-29 12:56:58 -0400

    trying static serving

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type']
 }))
 
-// app.use(express.static(path.join(__dirname, '../client/dist')))
+app.use(express.static(path.join(__dirname, '../client/dist')))
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
